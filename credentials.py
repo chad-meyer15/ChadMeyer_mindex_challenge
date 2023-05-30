6d0eebde016a07dc779e4a153fb06b26dd3a6414
Chad Meyer <chadmeyer@Chads-MBP-2.lan>
2023-05-30 11:21:56 -0400

    Adding credentials file for future usability purposes

---
+++ credentials.py
@@ -0,0 +1,8 @@
+AccessKey = "AKIAZZ33YB65GZIN656A"
+SecretKey = "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"
+BucketName = 'mindex-data-analytics-code-challenge'
+DatabaseHost = "ls-2619b6b15c9bdc80a23f6afb7eee54cf0247da21.ca3yee6xneaj.us-east-1.rds.amazonaws.com"
+UserName = "chad_meyer"
+Password = "mhadceyer"
+TableName = "chad_meyer"
+DatabaseName = "postgres"
